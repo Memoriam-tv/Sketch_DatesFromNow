b3804310661a6f9638dd7c1b05627f4a6c39bd7e
Leon Berenschot <leon@wendbaar.nl>
2020-02-14 14:16:56 +0100

    VSCode linting

--- src/my-command.js
+++ src/my-command.js
@@ -1,23 +1,23 @@
-const sketch = require('sketch')
-const { DataSupplier } = sketch
-const util = require('util')
+const sketch = require("sketch");
+const { DataSupplier } = sketch;
+const util = require("util");
 
-export function onStartup () {
+export function onStartup() {
   // To register the plugin, uncomment the relevant type:
-  DataSupplier.registerDataSupplier('public.text', 'datademo', 'SupplyData')
+  DataSupplier.registerDataSupplier("public.text", "datademo", "SupplyData");
   // DataSupplier.registerDataSupplier('public.image', 'datademo', 'SupplyData')
 }
 
-export function onShutdown () {
+export function onShutdown() {
   // Deregister the plugin
-  DataSupplier.deregisterDataSuppliers()
+  DataSupplier.deregisterDataSuppliers();
 }
 
-export function onSupplyData (context) {
-  let dataKey = context.data.key
-  const items = util.toArray(context.data.items).map(sketch.fromNative)
+export function onSupplyData(context) {
+  let dataKey = context.data.key;
+  const items = util.toArray(context.data.items).map(sketch.fromNative);
   items.forEach((item, index) => {
-    let data = Math.random().toString()
-    DataSupplier.supplyDataAtIndex(dataKey, data, index)
-  })
+    let data = Math.random().toString();
+    DataSupplier.supplyDataAtIndex(dataKey, data, index);
+  });
 }
