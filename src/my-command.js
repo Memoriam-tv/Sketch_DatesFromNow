96a9ccf2e354d4cd4add64fcca0bbc3153c0d7ca
Leon Berenschot <leon@wendbaar.nl>
2020-02-14 14:21:38 +0100

    return a list of dates

--- src/my-command.js
+++ src/my-command.js
@@ -14,10 +14,45 @@ export function onShutdown() {
 }
 
 export function onSupplyData(context) {
+  const dayOfTheWeek = [
+    "Zondag",
+    "Maandag",
+    "Dinsdag",
+    "Woensdag",
+    "Donderdag",
+    "Vrijdag",
+    "Zaterdag"
+  ];
+  const monthNames = [
+    "January",
+    "Februari",
+    "Maart",
+    "April",
+    "Mei",
+    "Juni",
+    "Juli",
+    "Augustus",
+    "September",
+    "Oktober",
+    "November",
+    "December"
+  ];
+
   let dataKey = context.data.key;
   const items = util.toArray(context.data.items).map(sketch.fromNative);
+
+  var date = new Date();
+
   items.forEach((item, index) => {
-    let data = Math.random().toString();
+    let data =
+      dayOfTheWeek[date.getDay()] +
+      " " +
+      date.getDate() +
+      " " +
+      monthNames[date.getMonth()] +
+      " " +
+      date.getFullYear();
     DataSupplier.supplyDataAtIndex(dataKey, data, index);
+    date.setDate(date.getDate() + 1);
   });
 }
